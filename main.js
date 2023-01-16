e2818d40bef815ccb1bcc8c9904944bdd98651f8
LUIS <luis.esparzalz@uanl.edu.mx>
2023-01-16 13:41:28 -0600

    Clase 3 de Web, un poco de js y formulario en html

---
+++ main.js
@@ -0,0 +1,39 @@
+const Nombre = "Luis Angel Missael Esparza Lopez";
+const Datos = {ELO:"Diamante", Main:"No tengo", Carril: "Mid" };
+
+
+console.log(Datos);
+
+Datos.ELO = "Oro";
+Datos.Main = "Le Blanc";
+Datos.Carril = "Mid";
+
+console.log(Datos);
+
+var x  = 0; 
+x = "Hola";
+console.log(Nombre);
+console.log(x);
+console.log(Date.now() - 10000000000000);
+
+function FunctionOverTime(){
+
+    console.log(Date.now());
+    alert("Hola, las alerts no sirven para nada, no las usen");
+}
+
+FunctionOverTime();
+
+
+var M = 100
+console.log(M);
+
+
+{
+
+    let M = 200
+    console.log(M);
+
+}
+
+console.log(M);
